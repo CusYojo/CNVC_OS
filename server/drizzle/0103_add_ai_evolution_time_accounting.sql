89ea6e821d30e0240bfd053650122a1f6fcb5a25
yunmail0429-ctrl <yun.mail0429@gmail.com>
2026-09-15 12:13:37 +0800

    fix(evolution): split time accounting migration statements

--- server/drizzle/0103_add_ai_evolution_time_accounting.sql
+++ server/drizzle/0103_add_ai_evolution_time_accounting.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `sbl_ai_evolution_runs` ADD COLUMN `time_accounted_at` datetime(3) NULL;
+--> statement-breakpoint
 UPDATE `sbl_ai_evolution_runs` SET `time_accounted_at` = `updated_at`
 WHERE `status` IN ('preparing', 'executing', 'evaluating');
